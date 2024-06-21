6c3b69d2fbc4196dccbb074422c4deaeddf1982d
warolderoy <vincentdrolet@msn.com>
2024-06-21 16:26:34 -0400

    Created Maze class

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from graphics import Window
-from cell import Cell
+from maze import Maze
 
 def main():
     win = Window(800, 600)
-    cell1 = Cell(win)
-    cell2 = Cell(win)
-
-    cell1.has_right_wall = False
-    cell2.has_left_wall = False
-
-    cell1.draw(0, 0, 100, 100)
-    cell2.draw(100, 0, 200, 100)
-
-    cell1.draw_move(cell2, True)
+    maze = Maze(
+        50,
+        50,
+        10,
+        10,
+        30,
+        30,
+        win
+    )
+    
     
     win.wait_for_close()
 
---
+++ maze.py
@@ -0,0 +1,54 @@
+from cell import Cell
+from time import sleep
+
+class Maze:
+    def __init__(
+            self,
+            x1,
+            y1,
+            num_rows,
+            num_cols,
+            cell_size_x,
+            cell_size_y,
+            win
+    ):
+        self.__cells = []
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+        self.__cell_size_x = cell_size_x
+        self.__cel_size_y = cell_size_y
+        self.__win = win
+
+        self.__create_cells()
+        for i in range(len(self.__cells)):
+            for j in range(len(self.__cells[i])):
+                self.__draw_cell(i, j)
+
+
+    def __create_cells(self):
+        for _ in range(self.__num_cols):
+            col = []
+            for _ in range(self.__num_rows):
+                cell = Cell(self.__win)
+                col.append(cell)
+            self.__cells.append(col)
+    
+    def __draw_cell(self, i, j):
+        x_offset = i * self.__cell_size_x + self.__x1
+        y_offset = j * self.__cel_size_y + self.__y1
+
+        self.__cells[i][j].draw(
+            x_offset,
+            y_offset,
+            x_offset + self.__cell_size_x,
+            y_offset + self.__cel_size_y
+        )
+
+        self.__animate()
+    
+    def __animate(self):
+        self.__win.redraw()
+        sleep(0.05)
+        
