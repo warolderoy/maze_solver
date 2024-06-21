bb81dcbdcb54a0f829a70e7004d28f6926a6458a
warolderoy <vincentdrolet@msn.com>
2024-06-21 13:08:22 -0400

    Added Point and Line classes. Implemented line drawing.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     win = Window(800, 600)
+    p1 = Point(100, 100)
+    p2 = Point(200, 200)
+    line = Line(p1, p2)
+
+    win.draw_line(line, "black")
     win.wait_for_close()
 
 main()
