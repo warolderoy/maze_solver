0c5127fd9894f770a69eb96f3eff6f11b96d4b24
warolderoy <vincentdrolet@msn.com>
2024-06-21 14:19:05 -0400

    Corners of cells are now added when calling draw, instead of in the constructor

--- cell.py
+++ cell.py
@@ -1,18 +1,23 @@
 from graphics import Point, Line
 
 class Cell:
-    def __init__(self, x1, y1, x2, y2, win):
+    def __init__(self, win):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+        self.__x1 = None
+        self.__y1 = None
+        self.__x2 = None
+        self.__y2 = None
+        self.__win = win
+    
+    def draw(self, x1, y1, x2, y2):
         self.__x1 = x1
         self.__y1 = y1
         self.__x2 = x2
         self.__y2 = y2
-        self.__win = win
-    
-    def draw(self):
+
         top_left = Point(self.__x1, self.__y1)
         bottom_right = Point(self.__x2, self.__y2)
 
--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from cell import Cell
 
 def main():
     win = Window(800, 600)
-    cell1 = Cell(0, 0, 100, 100, win)
-    cell2 = Cell(100, 0, 200, 100, win)
+    cell1 = Cell(win)
+    cell2 = Cell(win)
 
     cell1.has_right_wall = False
     cell2.has_left_wall = False
 
-    cell1.draw()
-    cell2.draw()
+    cell1.draw(0, 0, 100, 100)
+    cell2.draw(100, 0, 200, 100)
     
     win.wait_for_close()
 
