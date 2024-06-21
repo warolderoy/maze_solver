f41c3f02b61a56d9c4547083baf9bcd657fd4786
warolderoy <vincentdrolet@msn.com>
2024-06-20 22:31:57 -0400

    Added main, implemented window

--- Window.py
+++ Window.py
@@ -1,3 +1,29 @@
 from tkinter import Tk, BOTH, Canvas
 # Remember to use python 3.10 to call
 
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title = "Maze Solver"
+        self.canvas = Canvas(
+            master=self.__root,
+            height=height,
+            width=width
+        )
+        self.canvas.pack()
+        self.running = False
+
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.running = True
+        
+        while self.running:
+            self.redraw()
+    
+    def close(self):
+        self.running = False
---
+++ main.py
@@ -0,0 +1,7 @@
+from Window import Window
+
+def main():
+    win = Window(800, 600)
+    win.wait_for_close()
+
+main()
