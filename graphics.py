c9d5b443e31ea9973214e68e31132a311f167542
warolderoy <vincentdrolet@msn.com>
2024-06-20 22:50:06 -0400

    Window update

--- graphics.py
+++ graphics.py
@@ -4,14 +4,18 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
-        self.__root.title = "Maze Solver"
-        self.canvas = Canvas(
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(
             master=self.__root,
+            bg="white",
             height=height,
             width=width
         )
-        self.canvas.pack()
-        self.running = False
+        self.__canvas.pack(
+            fill=BOTH,
+            expand=1
+        )
+        self.__running = False
 
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -20,10 +24,11 @@ class Window:
         self.__root.update()
 
     def wait_for_close(self):
-        self.running = True
+        self.__running = True
         
-        while self.running:
+        while self.__running:
             self.redraw()
+        print("Window closed successfully")
     
     def close(self):
-        self.running = False
+        self.__running = False
