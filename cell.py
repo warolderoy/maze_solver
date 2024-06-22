600ca9af4bc0aeed74fd5d693a35efc6e1609873
warolderoy <vincentdrolet@msn.com>
2024-06-22 12:17:54 -0400

    Added tests, refactored classes to use protected members instead of private

--- cell.py
+++ cell.py
@@ -6,39 +6,39 @@ class Cell:
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self.__x1 = None
-        self.__y1 = None
-        self.__x2 = None
-        self.__y2 = None
-        self.__win = win
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+        self._win = win
     
     def draw(self, x1, y1, x2, y2):
-        self.__x1 = x1
-        self.__y1 = y1
-        self.__x2 = x2
-        self.__y2 = y2
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
 
-        top_left = Point(self.__x1, self.__y1)
-        bottom_right = Point(self.__x2, self.__y2)
+        top_left = Point(self._x1, self._y1)
+        bottom_right = Point(self._x2, self._y2)
 
-        top_right = Point(self.__x2, self.__y1)
-        bottom_left = Point(self.__x1, self.__y2)
+        top_right = Point(self._x2, self._y1)
+        bottom_left = Point(self._x1, self._y2)
 
         if self.has_left_wall:
             left_wall = Line(top_left, bottom_left)
-            self.__win.draw_line(left_wall)
+            self._win.draw_line(left_wall)
 
         if self.has_right_wall:
             right_wall = Line(top_right, bottom_right)
-            self.__win.draw_line(right_wall)
+            self._win.draw_line(right_wall)
         
         if self.has_top_wall:
             top_wall = Line(top_left, top_right)
-            self.__win.draw_line(top_wall)
+            self._win.draw_line(top_wall)
         
         if self.has_bottom_wall:
             bottom_wall = Line(bottom_left, bottom_right)
-            self.__win.draw_line(bottom_wall)
+            self._win.draw_line(bottom_wall)
 
     def draw_move(self, to_cell, undo=False):
         if undo:
@@ -46,12 +46,12 @@ class Cell:
         else:
             line_color = "red"
         
-        if self.__x1 == None:
+        if self._x1 == None:
             raise Exception("cell not drawn")
-        if to_cell.__x1 == None:
+        if to_cell._x1 == None:
             raise Exception("to_cell not drawn")
         
-        p1 = Point((self.__x1+self.__x2)//2, (self.__y1+self.__y2)//2)
-        p2 = Point((to_cell.__x1+to_cell.__x2)//2, (to_cell.__y1+to_cell.__y2)//2)
+        p1 = Point((self._x1+self._x2)//2, (self._y1+self._y2)//2)
+        p2 = Point((to_cell._x1+to_cell._x2)//2, (to_cell._y1+to_cell._y2)//2)
 
-        self.__win.draw_line(Line(p1, p2), line_color)
+        self._win.draw_line(Line(p1, p2), line_color)
--- maze.py
+++ maze.py
@@ -12,50 +12,50 @@ class Maze:
             cell_size_y,
             win = None
     ):
-        self.__cells = []
-        self.__x1 = x1
-        self.__y1 = y1
-        self.__num_rows = num_rows
-        self.__num_cols = num_cols
-        self.__cell_size_x = cell_size_x
-        self.__cel_size_y = cell_size_y
-        self.__win = win
+        self._cells = []
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cel_size_y = cell_size_y
+        self._win = win
 
         self.__create_cells()
 
 
     def __create_cells(self):
-        for _ in range(self.__num_cols):
+        for _ in range(self._num_cols):
             col = []
-            for _ in range(self.__num_rows):
-                cell = Cell(self.__win)
+            for _ in range(self._num_rows):
+                cell = Cell(self._win)
                 col.append(cell)
-            self.__cells.append(col)
+            self._cells.append(col)
         
-        for i in range(len(self.__cells)):
-            for j in range(len(self.__cells[i])):
+        for i in range(len(self._cells)):
+            for j in range(len(self._cells[i])):
                 self.__draw_cell(i, j)
     
     def __draw_cell(self, i, j):
-        if self.__win == None:
+        if self._win == None:
             return
         
-        x_offset = i * self.__cell_size_x + self.__x1
-        y_offset = j * self.__cel_size_y + self.__y1
+        x_offset = i * self._cell_size_x + self._x1
+        y_offset = j * self._cel_size_y + self._y1
 
-        self.__cells[i][j].draw(
+        self._cells[i][j].draw(
             x_offset,
             y_offset,
-            x_offset + self.__cell_size_x,
-            y_offset + self.__cel_size_y
+            x_offset + self._cell_size_x,
+            y_offset + self._cel_size_y
         )
 
         self.__animate()
     
     def __animate(self):
-        if self.__win == None:
+        if self._win == None:
             return
         
-        self.__win.redraw()
+        self._win.redraw()
         sleep(0.05)
         
