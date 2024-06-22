0cacdce9df214fb1b523755cc61f29baad885f8b
warolderoy <vincentdrolet@msn.com>
2024-06-22 12:32:27 -0400

    Added tests

--- tests.py
+++ tests.py
@@ -15,6 +15,67 @@ class Tests(unittest.TestCase):
             len(m1._cells[0]),
             num_rows,
         )
+    
+    def test_maze_no_rows(self):
+        num_cols = 12
+        num_rows = 0
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+    
+    def test_maze_no_cols(self):
+        num_cols = 0
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+    
+    def test_maze_one_row(self):
+        num_cols = 12
+        num_rows = 1
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+    
+    def test_maze_one_col(self):
+        num_cols = 1
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+    
+    def test_maze_big(self):
+        num_cols = 100
+        num_rows = 100
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
 
 if __name__ == "__main__":
     unittest.main()
